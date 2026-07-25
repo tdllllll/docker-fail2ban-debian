45d7b9d581db142f5d5a25d14f13a271f8685bdc
tdllllll <78018413+tdllllll@users.noreply.github.com>
2026-07-26 07:33:38 +0800

    Change compose logic

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 ## docker-fail2ban-debian
 
-This is an experimental repository based on [crazy-max/docker-fail2ban](https://github.com/crazy-max/docker-fail2ban). It is modified to be Debian-based and uses the apt `fail2ban` package which contains os-specific configurations (e.g. defaults-debian.conf). It also supports integration with `systemd-journald`.
+This is an experimental repository based on [crazy-max/docker-fail2ban](https://github.com/crazy-max/docker-fail2ban). It is modified to be Debian-based and uses the apt-managed `fail2ban` package which contains os-specific configurations (e.g. `defaults-debian.conf`). So it's useful for Debian hosts. It also supports integration with `systemd-journald`.
 
-Most files are unchanged from upstream. Please refer to their [README.md](https://github.com/crazy-max/docker-fail2ban/blob/master/README.md) for more details.
+Most logic are unchanged from upstream. Please refer to their [README.md](https://github.com/crazy-max/docker-fail2ban/blob/master/README.md) for more details.
 
-WARNING: Performance, disk usage, functionality and stability are not guaranteed to be identical to upstream.
+WARNING: Performance, disk usage, functionality, stability, security, etc. are not guaranteed to be identical to upstream.
 
 ## Changes
 
-1. `read_only` is enabled, so any specified `F2B_LOG_TARGET` will be written to `/data` directory.
+1. A `read_only` file system is enabled.
+  - So any specified `F2B_LOG_TARGET` will be written to `/data` directory.
+  - After configurations are changed you will need to rerun `docker compose up -d` instead of just restarting.
+  - `fail2ban.env` was removed. You now need to specify each variable at the `arg` section in `compose.yml`.
+  - A new variable `F2B_NO_DEB_DEFAULTS` was added. You can set any non-empty value to `F2B_NO_DEB_DEFAULTS` then `defaults-debian.conf` will be removed.
 
 ## Credits
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,81 +1,32 @@
 #!/bin/bash
 
-TZ=${TZ:-UTC}
-
-F2B_LOG_TARGET=${F2B_LOG_TARGET:-STDOUT}
-F2B_LOG_LEVEL=${F2B_LOG_LEVEL:-INFO}
-F2B_DB_PURGE_AGE=${F2B_DB_PURGE_AGE:-1d}
-IPTABLES_MODE=${IPTABLES_MODE:-auto}
+if [ "${F2B_LOG_LEVEL}" == "DEBUG" ]; then
+  echo "TZ: ${TZ}"
+  echo "F2B_LOG_TARGET: ${F2B_LOG_TARGET}"
+  echo "F2B_LOG_LEVEL: ${F2B_LOG_LEVEL}"
+  echo "F2B_DB_PURGE_AGE: ${F2B_DB_PURGE_AGE}"
+  echo "IPTABLES_MODE: ${IPTABLES_MODE}"
+  echo "F2B_NO_DEB_DEFAULTS: ${F2B_NO_DEB_DEFAULTS}"
+fi
 
 # Timezone
-echo "Setting timezone to ${TZ}..."
-ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime
-echo ${TZ} > /etc/timezone
+if [ "$(cat /etc/timezone)" != "${TZ}" ]; then
+  echo "WARNING: timezone mismatched"
+fi
 
 # Init
-echo "Initializing files and folders..."
-mkdir -p /data/run /data/lib /data/db /data/action.d /data/filter.d /data/jail.d
-
-# Fail2ban conf
-echo "Setting Fail2ban configuration..."
-sed -i "s|logtarget =.*|logtarget = /data/${F2B_LOG_TARGET#/}|g" /etc/fail2ban/fail2ban.conf
-sed -i "s/loglevel =.*/loglevel = $F2B_LOG_LEVEL/g" /etc/fail2ban/fail2ban.conf
-sed -i "s|dbfile =.*|dbfile = /data/db/fail2ban\.sqlite3|g" /etc/fail2ban/fail2ban.conf
-sed -i "s/dbpurgeage =.*/dbpurgeage = $F2B_DB_PURGE_AGE/g" /etc/fail2ban/fail2ban.conf
-sed -i "s/#allowipv6 =.*/allowipv6 = auto/g" /etc/fail2ban/fail2ban.conf
-
-# Check custom actions
-echo "Checking for custom actions in /data/action.d..."
-actions=$(ls -l /data/action.d | grep -E '^-' | awk '{print $9}')
-for action in ${actions}; do
-  if [ -f "/etc/fail2ban/action.d/${action}" ]; then
-    echo "  WARNING: ${action} already exists and will be overriden"
-    rm -f "/etc/fail2ban/action.d/${action}"
-  fi
-  echo "  Add custom action ${action}..."
-  ln -sf "/data/action.d/${action}" "/etc/fail2ban/action.d/"
-done
-
-# Check custom filters
-echo "Checking for custom filters in /data/filter.d..."
-filters=$(ls -l /data/filter.d | grep -E '^-' | awk '{print $9}')
-for filter in ${filters}; do
-  if [ -f "/etc/fail2ban/filter.d/${filter}" ]; then
-    echo "  WARNING: ${filter} already exists and will be overriden"
-    rm -f "/etc/fail2ban/filter.d/${filter}"
-  fi
-  echo "  Add custom filter ${filter}..."
-  ln -sf "/data/filter.d/${filter}" "/etc/fail2ban/filter.d/"
-done
-
-# Check custom jails
-echo "Checking for custom jails in /data/jail.d..."
-filters=$(ls -l /data/jail.d | grep -E '^-' | awk '{print $9}')
-for filter in ${filters}; do
-  if [ -f "/etc/fail2ban/jail.d/${filter}" ]; then
-    echo "  WARNING: ${filter} already exists and will be overriden"
-    rm -f "/etc/fail2ban/jail.d/${filter}"
-  fi
-  echo "  Add custom jail ${filter}..."
-  ln -sf "/data/jail.d/${filter}" "/etc/fail2ban/jail.d/"
-done
+if [ ! -d "/data/run" ] || [ ! -d "/data/lib" ] || [ ! -d "/data/db" ]; then
+  echo "WARNING: missing folders"
+fi
 
 if [ "$IPTABLES_MODE" = "auto" ] && ! iptables -L &> /dev/null; then
   echo "WARNING: iptables is not supported by the host"
-elif [ "$IPTABLES_MODE" = "nft" ]; then
-  echo "WARNING: iptables-nft enforced"
-  update-alternatives --set iptables /usr/sbin/iptables-nft
-  update-alternatives --set ip6tables /usr/sbin/ip6tables-nft
-elif [ "$IPTABLES_MODE" = "legacy" ]; then
-  echo "WARNING: iptables-legacy enforced"
-  update-alternatives --set iptables /usr/sbin/iptables-legacy
-  update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 fi
 
 iptables -V
 nft -v
 
-if ! command -v journalctl &> /dev/null; then
+if [ ! command -v journalctl &> /dev/null ]; then
   echo "WARNING: systemd-journalctl not found"
 fi
 
